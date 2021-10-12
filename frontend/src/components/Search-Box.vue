0b075ea7fdcfa2e69257e8766c50edf5a318dc7d
Ingar Helgesen <ingar@i-h.no>
2021-10-12 14:22:18 +0200

    Add vant framework and start of dashboard

---
+++ frontend/src/components/Search-Box.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="Search-Box">
+    <!-- <van-search
+      id="searchField"
+      v-model="searchText"
+      placeholder="Søk på navn, ID, fosterhjem.."
+      shape="round"
+      background="#FFFFFF00"
+    /> -->
+    <van-field
+      v-model="message"
+      label=""
+      placeholder="Søk på navn, ID, fosterhjem.."
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'SearchBox',
+  data: () => ({
+    searchText: ''
+  })
+})
+</script>
+
+<style>
+#searchField {
+  color: rgba(0, 0, 0, 128);
+}
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <div class="background" />
+    <van-row justify="center">
+      <van-col span="4" class="logo">
+        <img alt="Redd-dyra logo" src="../assets/redd-dyra-logo.png">
+      </van-col>
+      <van-col span="16" class="logo">
+        <h1>REDD DYRA</h1>
+      </van-col>
+    </van-row>
+    <van-row justify="center">
+      <van-col span="18" class="logo">
+        <Search-Box />
+      </van-col>
+    </van-row>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import SearchBox from '../components/Search-Box.vue'
 
 export default defineComponent({
   name: 'Home',
   components: {
-    HelloWorld
+    SearchBox
   }
 })
 </script>
+
+<style scoped>
+.background {
+  background: url('/img/cat-wallpaper.jpg') no-repeat;
+  background-position: -520px 0px;
+  filter: blur(10px);
+  height: 100%;
+  width: 100%;
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  z-index: -1;
+}
+.logo {
+  font-size: 1.3em;
+  text-align: center;
+}
+.logo img {
+  padding-top: 16px; /* TODO: Is there a way to calculate this? */
+}
+</style>
