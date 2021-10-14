39a28f4f22028a24f0387becd35baf1756f0a171
Ingar Helgesen <ingar@i-h.no>
2021-10-15 00:07:45 +0200

    feat(home): add dashboard cards

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div>
     <Background />
     <div class="flex-column">
-      <div class="flex-row">
+      <div class="flex-row logo-row">
         <div>
           <img alt="Redd-dyra logo" src="../assets/redd-dyra-logo.png">
         </div>
@@ -12,6 +12,13 @@
       </div>
       <div class="flex-row">
         <Search-Box id="search-box" />
+      </div>
+      <div class="flex-row card-row">
+        <div class="card" v-for="card in cards" :key="card.title">
+          <img class="card-icon" alt="House icon" :src="card.icon">
+          <span class="card-title">{{card.title}}</span>
+          <span class="card-status">{{card.status}}</span>
+        </div>
       </div>
     </div>
   </div>
@@ -27,7 +34,31 @@ export default defineComponent({
   components: {
     SearchBox,
     Background
-  }
+  },
+  data: () => ({
+    cards: [
+      {
+        title: 'Kattehuset',
+        icon: '/img/home/house-icon.png',
+        status: '>🏠 2 | 🏥 1 | 🏠 4'
+      },
+      {
+        title: 'Dagslogger',
+        icon: '/img/home/note-icon.png',
+        status: 'Dagens er påbegynt'
+      },
+      {
+        title: 'Oppgaver',
+        icon: '/img/home/checklist-icon.png',
+        status: '4 oppgaver gjenstår'
+      },
+      {
+        title: 'Arbeidsvakter',
+        icon: '/img/home/clock-icon.png',
+        status: '2 dager ubemannet'
+      }
+    ]
+  })
 })
 </script>
 
@@ -39,12 +70,49 @@ export default defineComponent({
 }
 
 .flex-row {
-  width: 100%;
+  width: 85%;
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
   padding-top: 30px;
+}
+
+.logo-row {
+  width: 90%
+}
+
+.card-row {
+  flex-wrap: wrap;
+  /* justify-content: space-between; */
+  row-gap: 20px;
+  column-gap: 20px;
+}
+
+.card {
+  background-color: rgba(255, 255, 255, 0.75);
+  height: 135px;
+  width: 135px;
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.card-icon {
+  width: 60%;
+  padding-top: 2px;
+}
+
+.card-title {
+  font-size: 1.2em;
+  font-weight: bold;
+}
+
+.card-status {
+  font-size: 0.8em;
+  padding: 0px 10px 5px 10px;
 }
 
 #logo-text {
@@ -56,7 +124,7 @@ export default defineComponent({
 }
 
 #search-box {
-  width: 80%;
+  width: 100%;
   max-width: 400px;
 }
 </style>
