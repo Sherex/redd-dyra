b1174212528a3db555075c7fe019a1bee1becf7d
Ingar Helgesen <ingar@i-h.no>
2021-10-16 17:12:43 +0200

    feat: add Search view at /search

---
+++ frontend/src/views/Search.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex-column">
+    <div class="flex-row">
+      <Search-Box id="search-box" />
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import SearchBox from '../components/Search-Box.vue'
+
+export default defineComponent({
+  name: 'Search',
+  components: {
+    SearchBox
+  }
+})
+</script>
