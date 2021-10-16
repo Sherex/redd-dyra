5a0a3ae7e8c286abac6da630672adcdefd1bdf23
Ingar Helgesen <ingar@i-h.no>
2021-10-16 16:15:40 +0200

    feat(search-box): add dashboard icon on non-root urls

--- frontend/src/components/Search-Box.vue
+++ frontend/src/components/Search-Box.vue
@@ -1,24 +1,37 @@
 <template>
   <div>
     <div id="parent">
+      <ViewDashboardOutline id="dashboard-icon" class="icon"  v-if="currentRoute !== '/'" @click="routeToDashboard" />
       <input id="search-field" type="search" placeholder="Søk på navn, ID, fosterhjem.." v-model="searchText">
-      <Magnify class="icon" />
+      <Magnify id="search-icon" class="icon" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { Magnify } from 'mdue'
+import { Magnify, ViewDashboardOutline } from 'mdue'
+import router from '../router'
 
 export default defineComponent({
   name: 'SearchBox',
   components: {
-    Magnify
+    Magnify,
+    ViewDashboardOutline
   },
   data: () => ({
     searchText: ''
-  })
+  }),
+  methods: {
+    routeToDashboard: () => {
+      router.push('/')
+    }
+  },
+  computed: {
+    currentRoute: () => {
+      return router.currentRoute.value.path
+    }
+  }
 })
 </script>
 
@@ -31,14 +44,23 @@ export default defineComponent({
 }
 
 #search-field {
-  background-color: rgba(0, 0, 0, 0);
+  background-color: transparent;
   outline: none;
   border: none;
   width: 100%;
   padding: 10px 0px 10px 15px;
 }
 
-.icon {
+#dashboard-icon + #search-field {
+  border-left: #7E7E7E 1px solid;
+  padding-left: 5px;
+}
+
+#search-icon {
   padding: 0px 10px 0px 5px;
+}
+
+#dashboard-icon {
+  padding: 0px 5px 0px 10px;
 }
 </style>
