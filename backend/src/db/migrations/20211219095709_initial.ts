32f3d0c4ce23b2921a55a1ecad8aaf4df7f253ea
Ingar Helgesen <ingar@i-h.no>
2021-12-21 23:32:07 +0100

    fix(db): fix columns and down migration

--- backend/src/db/migrations/20211219095709_initial.ts
+++ backend/src/db/migrations/20211219095709_initial.ts
@@ -8,23 +8,26 @@ function tableTemplate (knex: Knex, table: Knex.TableBuilder, include?: Template
 }
 
 export async function up (knex: Knex): Promise<void> {
-  await knex.schema.raw('CREATE EXTENSION "uuid-ossp"')
-  await knex.schema.raw('CREATE EXTENSION pgmemento')
+  await knex.schema.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+  await knex.schema.raw('CREATE EXTENSION IF NOT EXISTS "pgmemento"')
+
   await knex.schema.withSchema('public').createTable('user', table => {
     tableTemplate(knex, table)
     table.timestamp('deletedAt')
     table.text('email').notNullable().unique()
     table.text('passwordHash').notNullable()
     table.text('name').notNullable()
-    table.integer('invitedByUserId').notNullable().references('id').inTable('user')
+    table.integer('invitedByUserId').references('id').inTable('user')
   })
+
   await knex.schema.withSchema('public').createTable('user_session', table => {
     tableTemplate(knex, table, ['id', 'createdAt'])
     table.integer('expiresAfterSeconds').notNullable()
-    table.integer('invitedByUserId').notNullable().references('id').inTable('user')
-    table.uuid('token').notNullable().unique()
+    table.integer('userId').references('id').inTable('user')
+    table.uuid('token').notNullable().unique().defaultTo(knex.raw('uuid_generate_v1mc()'))
     table.text('deviceType')
   })
+
   await knex.schema.withSchema('public').createTable('join_token', table => {
     table.uuid('id').primary().defaultTo(knex.raw('uuid_generate_v1mc()'))
     tableTemplate(knex, table, ['createdAt'])
@@ -33,5 +36,9 @@ export async function up (knex: Knex): Promise<void> {
 }
 
 export async function down (knex: Knex): Promise<void> {
+  await knex.schema.withSchema('public').dropTableIfExists('user_session')
+  await knex.schema.withSchema('public').dropTableIfExists('join_token')
   await knex.schema.withSchema('public').dropTableIfExists('user')
+  await knex.schema.raw('DROP EXTENSION IF EXISTS "uuid-ossp"')
+  await knex.schema.raw('DROP EXTENSION IF EXISTS "pgmemento"')
 }
