ae208f9f5119d4f32a13e8214571e0be742e1058
Ingar Helgesen <ingar@i-h.no>
2021-12-21 16:27:00 +0100

    refactor: create and use getCurrentPath util

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,22 +2,17 @@ import 'reflect-metadata'
 import { buildSchema } from 'type-graphql'
 import { ApolloServer } from 'apollo-server'
 import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core'
-import { fileURLToPath } from 'url'
-import { dirname, resolve as pathResolve } from 'path'
 import { resolvers } from './schema/index.js'
 import { createContext } from './schema/context.js'
 import { logger } from './lib/logger.js'
 import { config } from './config.js'
+import { getCurrentPath } from './lib/utils.js'
 
 logger.info(`Starting backend in a ${config.inDev ? 'development' : 'production'} environment`)
 
-// Get the directory of this file (index.ts)
-const __filename = fileURLToPath(import.meta.url) // eslint-disable-line @typescript-eslint/naming-convention
-const __dirname = dirname(__filename) // eslint-disable-line @typescript-eslint/naming-convention
-
 const schema = await buildSchema({
   resolvers: resolvers,
-  emitSchemaFile: pathResolve(__dirname, 'schema.gql')
+  emitSchemaFile: getCurrentPath(import.meta, 'schema.gql').resolvedPath
 })
 
 const server = new ApolloServer({
--- backend/src/lib/utils.ts
+++ backend/src/lib/utils.ts
@@ -1,1 +1,25 @@
+import { fileURLToPath } from 'url'
+import { dirname, resolve as pathResolve } from 'path'
+
 export const delay = async (ms: number): Promise<void> => await new Promise(resolve => setTimeout(resolve, ms))
+
+interface GetCurrentPathReturn {
+  __filename: string
+  __dirname: string
+  resolvedPath?: string
+}
+
+export function getCurrentPath (importMeta: ImportMeta): Omit<GetCurrentPathReturn, 'resolvedPath'>
+export function getCurrentPath (importMeta: ImportMeta, resolveTo: string): Required<GetCurrentPathReturn>
+export function getCurrentPath (importMeta: ImportMeta, resolveTo?: string): GetCurrentPathReturn {
+  const __filename = fileURLToPath(importMeta.url) // eslint-disable-line @typescript-eslint/naming-convention
+  const __dirname = dirname(__filename) // eslint-disable-line @typescript-eslint/naming-convention
+
+  const resolvedPath = typeof resolveTo === 'string' ? pathResolve(__dirname, resolveTo) : undefined
+
+  return {
+    __filename,
+    __dirname,
+    resolvedPath
+  }
+}
